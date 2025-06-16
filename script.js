91a875f945b9472c9dd7ae442a010bd4a049f955
shashwat <141143239+Khatry-With-A-Y@users.noreply.github.com>
2025-06-16 13:57:17 +0000

    Add function getComputerChoice() to random choice
    Add function getComputerChoice() to random choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    rand = Math.random();
+    if (rand >= 0 && rand <= 0.33) {
+        string = "rock";
+    }
+    if (rand >= 0.34 && rand <= 0.66) {
+        string = "paper";
+    }
+    if (rand >= 0.67 && rand <= 1) {
+        string = "scissors";
+    }
+
+    return string;
+}
