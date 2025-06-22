21639b005ddb3d1fe7036d2a35e6652ce3afc0e8
shashwat <141143239+Khatry-With-A-Y@users.noreply.github.com>
2025-06-22 11:35:21 +0000

    Write the logic to play a single round using function playRound();

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
 function getComputerChoice() {
     rand = Math.random();
     if (rand >= 0 && rand <= 0.33) {
-        string = "rock";
+        computerChoice = "rock";
     }
     if (rand >= 0.34 && rand <= 0.66) {
-        string = "paper";
+        computerChoice = "paper";
     }
     if (rand >= 0.67 && rand <= 1) {
-        string = "scissors";
+        computerChoice = "scissors";
     }
 
-    return string;
+    return computerChoice;
 }
 
 function getHumanChoice(){
-    choice = window.prompt("Think fast chuckle nuts! (rock, paper, or scissors?)");
+    humanChoice = window.prompt("Think fast chuckle nuts! (rock, paper, or scissors?)");
 
-    return choice;
+    return humanChoice;
 }
+
+var humanScore = 0;
+var computerScore = 0;
+
+function playRound (humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    
+    if(humanChoice === "rock"){
+        if(computerChoice === "rock"){
+            result = "Oops! It's a draw!";
+        }
+        else if(computerChoice === "paper"){
+            computerScore++;
+            result = "You lose! Paper beats rock!";
+        }else{
+            humanScore++;
+            result = "You win! Rock beasts scissors!";
+        }
+    }
+    else if(humanChoice === "paper"){
+        if(computerChoice === "paper"){
+            result = "Oops! It's a draw!";
+        }
+        else if(computerChoice === "scissors"){
+            computerScore++;
+            result = "You lose! Scissors beats paper!";
+        }else{
+            humanScore++;
+            result = "You win! Paper beats rock!"
+        }
+    }
+    else if(humanChoice === "scissors"){
+        if(computerChoice === "scissors"){
+            result = "Oops! It's a draw!";
+        }
+        else if(computerChoice === "rock"){
+            computerScore++;
+            result = "You lose! Rock beats scissors!";
+        }else{
+            humanScore++;
+            result = "You win! Scissors beats paper!"
+        }
+    }
+    console.log(result);
+    return result;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
